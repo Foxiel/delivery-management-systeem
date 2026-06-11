718cffbfe09dcba9e9ed4ecf6cc92c4cf70ae2f2
foxie <fabianverhagen2011@gmail.com>
2026-06-11 14:49:54 +0200

    Initial commit

---
+++ delivery-management-systeem/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace delivery_management_systeem;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ delivery-management-systeem/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace delivery_management_systeem;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
